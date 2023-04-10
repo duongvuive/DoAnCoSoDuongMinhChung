d4d56199f9a89d15072bac9ffab76a2083750468
Hoài Dương Võ <vohoaiduong2015@gmail.com>
2023-04-10 14:07:12 +0700

    thêm thông tin khách hàng

--- WebBanHangOnline/Controllers/ThongTinKhachHangController.cs
+++ WebBanHangOnline/Controllers/ThongTinKhachHangController.cs
@@ -8,13 +8,14 @@ using System.Web;
 using System.Web.Helpers;
 using System.Web.Mvc;
 using WebBanHangOnline.Models;
-using WebBanHangOnline.Models.EF;
+using WebBanHangOnline.Models.SQL;
 
 namespace WebBanHangOnline.Controllers
 {
     public class ThongTinKhachHangController : Controller
     {
         // GET: ThongTinKhachHang
+       dbWebBanHang db=new dbWebBanHang();
         public ActionResult ProfileKH()
         {
             // Lấy thông tin của người dùng hiện tại
@@ -42,6 +43,45 @@ namespace WebBanHangOnline.Controllers
             };
 
             return View(model);
+        }
+        public ActionResult ThemThongTinKH()
+        {
+            List<City> cityList = db.Cities.ToList();
+            ViewBag.City = new SelectList(cityList, "CityId", "Name");
+            return View();
+        }
+ 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ThemThongTinKH([Bind(Include = "FullName,Phone,Address,City,District,Ward")] Customer customer)
+        {
+            string AccountID = User.Identity.GetUserId();
+            if (!ModelState.IsValid)
+            {
+                customer.CustomerID = Guid.NewGuid().ToString();
+                customer.Id = AccountID;
+                customer.DateCreated = DateTime.Now;
+                customer.LastUpdated = DateTime.Now;
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+
+            var cityList = db.Cities.ToList();
+            ViewBag.City = new SelectList(cityList, "CityId", "Name");
+            return View(customer);
+        }
+        public ActionResult GetDistrictsByCityId(int cityId)
+        {
+            List<District> districts = db.Districts.Where(m => m.CityId == cityId).ToList();
+            ViewBag.District = new SelectList(districts, "DistrictId", "Name");
+            return PartialView("DistrictState");
+        }
+        public ActionResult GetWardsByDistrictId(int DistrictId)
+        {
+            List<Ward>  wards= db.Wards.Where(m => m.DistrictId == DistrictId).ToList();
+            ViewBag.Ward = new SelectList(wards, "DistrictId", "Name");
+            return PartialView("WardState");
         }
 
     }
